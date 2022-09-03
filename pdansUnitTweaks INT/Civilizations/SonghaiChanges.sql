8ba1c79788094e081adedb3ab82e7c58abee6f6b
hyuckkim <monkeyhyuck@naver.com>
2022-09-03 14:13:48 +0900

    pdanunittweaks int v33

--- pdansUnitTweaks INT/Civilizations/SonghaiChanges.sql
+++ pdansUnitTweaks INT/Civilizations/SonghaiChanges.sql
@@ -1,7 +1,3 @@
 UPDATE Language_ko_KR
-SET Text = '[COLOR_POSITIVE_TEXT]승선[ENDCOLOR]한 유닛의 방어력 보너스가 50% 증가합니다.'
-WHERE Tag = 'TXT_KEY_PROMOTION_WAR_CANOES_HELP';
-
-UPDATE Language_ko_KR
-SET Text = '야만인 주둔지나 도시를 약탈할 때 획득하는 [ICON_GOLD]골드가 [COLOR_POSITIVE_TEXT]세 배[ENDCOLOR]로 증가합니다. 지상 유닛이 [COLOR_PLAYER_PURPLE]전투용 카누[ENDCOLOR] 및 [COLOR_PLAYER_PURPLE]수륙 양용[ENDCOLOR] 승급을 보유합니다. [COLOR_POSITIVE_TEXT]강[ENDCOLOR]을 [ICON_CONNECTED]도시 연결에 사용할 수 있습니다.'
+SET Text = '야만인 주둔지나 도시를 약탈할 때 획득하는 [ICON_GOLD]골드가 [COLOR_POSITIVE_TEXT]세 배[ENDCOLOR]로 증가합니다. 지상 유닛이 [COLOR_PLAYER_PURPLE]수륙 양용[ENDCOLOR] 승급을 보유합니다. [COLOR_POSITIVE_TEXT]강[ENDCOLOR]을 [ICON_CONNECTED]도시 연결에 사용할 수 있습니다.'
 WHERE Tag = 'TXT_KEY_TRAIT_AMPHIB_WARLORD';
