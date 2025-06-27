c32300866af3291713f51ff5575eddca9025f0d5
HANDANI <goohandani@gmail.com>
2025-06-28 00:21:11 +0900

    한국어 미표시 문제 해결, 세계 의회 용어 단일화
    한국어 미표시 문제 해결:
    - More Wonder
    - Promotion Overhaul
    - Even More Resources UCS
    - JFD's Sovereignty
    - Bare Necessities
    
    세계 의회, 세계 대회로 혼용되던 용어를 세계 의회로 일관되게 변경되었습니다.

--- Vox Populi's Bare Necessities INT/Processes.sql
+++ Vox Populi's Bare Necessities INT/Processes.sql
@@ -1,24 +1,3 @@
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('TECHPRIEST_ATLAS', 256, 'FaithTourismAtlas256.dds', 8, 1);
-
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('TECHPRIEST_ATLAS', 128, 'FaithTourismAtlas128.dds', 8, 1);
-
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('TECHPRIEST_ATLAS', 80, 'FaithTourismAtlas80.dds', 8, 1);
-
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('TECHPRIEST_ATLAS', 64, 'FaithTourismAtlas64.dds', 8, 1);
-
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('TECHPRIEST_ATLAS', 45, 'FaithTourismAtlas45.dds', 8, 1);
-
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('TECHPRIEST_ATLAS', 32, 'FaithTourismAtlas32.dds', 8, 1);
-
-INSERT OR REPLACE INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn)
-VALUES ('REALLYBASED_ATLAS', 64, 'ReallyBased64.dds', 1, 3);
-
 INSERT OR REPLACE INTO LocalizedText
 	(Language, Tag, Text)
 VALUES
@@ -31,19 +10,3 @@ VALUES
 	('ko_KR', 'TXT_KEY_PROCESS_FAITH', '기도'),
 	('ko_KR', 'TXT_KEY_PROCESS_FAITH_HELP', '[COLOR_POSITIVE_TEXT]기도[ENDCOLOR]는 이 도시 [ICON_PRODUCTION] 생산량의 25%를 [ICON_PEACE] 신앙으로 전환하고, [ICON_RELIGION] 종교적 불안이 25% 감소합니다.'),
 	('ko_KR', 'TXT_KEY_PROCESS_FAITH_STRATEGY', '기도에 생산을 투자하면 도시의 신앙 생산량을 늘리는 데 도움이 되며, 선교사나 종교재판관이 필요하거나 도시에서 위인을 배출해야 할 때 유용합니다! 또한 종교 불안도 감소시킵니다.');
-
-
-INSERT OR REPLACE INTO Processes (Type, Description, Help, Strategy, TechPrereq, IconAtlas, PortraitIndex)
-SELECT 	'PROCESS_FAITH', 'TXT_KEY_PROCESS_FAITH', 'TXT_KEY_PROCESS_FAITH_HELP', 'TXT_KEY_PROCESS_FAITH_STRATEGY', 'TECH_PHILOSOPHY', 'TECHPRIEST_ATLAS', 0 UNION ALL
-SELECT	'PROCESS_GOLDEN_AGE', 'TXT_KEY_PROCESS_GOLDEN_AGE', 'TXT_KEY_PROCESS_GOLDEN_AGE_HELP', 'TXT_KEY_PROCESS_GOLDEN_AGE_STRATEGY', 'TECH_ECOLOGY', 'TECHPRIEST_ATLAS', 2;-- UNION ALL
---SELECT	'PROCESS_TOURISM', 'TXT_KEY_PROCESS_TOURISM', 'TXT_KEY_PROCESS_TOURISM_HELP', 'TXT_KEY_PROCESS_TOURISM_STRATEGY', NULL, 'TECHPRIEST_ATLAS', 1;
-
-INSERT OR REPLACE INTO Process_ProductionYields (ProcessType, YieldType, Yield)
-SELECT 'PROCESS_FAITH', 'YIELD_FAITH', 25 UNION ALL
-SELECT 'PROCESS_GOLDEN_AGE', 'YIELD_GOLDEN_AGE_POINTS', 25 UNION ALL
-SELECT 'PROCESS_GOLDEN_AGE', 'YIELD_GOLD', 5 UNION ALL
-SELECT 'PROCESS_GOLDEN_AGE', 'YIELD_SCIENCE', 5 UNION ALL
-SELECT 'PROCESS_GOLDEN_AGE', 'YIELD_CULTURE', 5 UNION ALL
-SELECT 'PROCESS_GOLDEN_AGE', 'YIELD_FOOD', 5 UNION ALL
-SELECT 'PROCESS_GOLDEN_AGE', 'YIELD_FAITH', 5;-- UNION ALL
---SELECT 'PROCESS_TOURISM', 'YIELD_TOURISM', 15;
