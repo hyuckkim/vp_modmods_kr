54cc83856f898ad047d875bb81ffe7534c689608
HANDANI <goohandani@gmail.com>
2025-07-03 01:19:12 +0900

    하나 빼먹음.

---
+++ Modbuddy/Recon Pay (Core1) Exploration Balancer (v 3) INT/Recon Pay (Core1) Exploration Balancer (v 3) INT/Promos.sql
@@ -0,0 +1,8 @@
+INSERT INTO Language_ko_KR
+        (Tag,										Text)
+VALUES	('TXT_KEY_PROMOTION_PATHFINDER',			'탐사 논리'),
+        ('TXT_KEY_PROMOTION_PATHFINDER_HELP',		'숲, 정글, 언덕, 눈, 사막, 습지에서의 [ICON_MOVES] 이동 비용을 무시합니다. 페널티 없이 [COLOR_WATER_TEXT]강[ENDCOLOR]을 건널 수 있습니다. 매 턴마다 근접 공격 1회를 [COLOR_SELECTED_TEXT]회피[ENDCOLOR]합니다.[NEWLINE]전투시 잃습니다.'),
+        ('TXT_KEY_PEDIA_PROMOTION_PATHFINDER',		'탐사 논리'),
+		('TXT_KEY_PROMOTION_PATHFINDER_2',			'전투 논리'),
+        ('TXT_KEY_PROMOTION_PATHFINDER_2_HELP',		'습지, 눈, 사막에서의 [ICON_MOVES] 이동 비용을 무시합니다. 적의 [COLOR_SELECTED_TEXT]통제 구역(ZOC)[ENDCOLOR]을 무시합니다.[NEWLINE]업그레이드시 소실됩니다.'),
+        ('TXT_KEY_PEDIA_PROMOTION_PATHFINDER_2',	'전투 논리');
