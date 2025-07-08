2860122ebd03365e62e791710c2cf53e55408053
HANDANI <goohandani@gmail.com>
2025-07-09 00:01:47 +0900

    오역 수정

--- Recon Pay (Xtra1) Arctician (v 2) INT/Promos.sql
+++ Recon Pay (Xtra1) Arctician (v 2) INT/Promos.sql
@@ -1,5 +1,5 @@
 INSERT INTO Language_ko_KR
         (Tag,													Text)
 VALUES	('TXT_KEY_PROMOTION_ARCTICIAN',							'극지인'),
-        ('TXT_KEY_PROMOTION_ARCTICIAN_HELP',					'[COLOR_WATER_TEXT]빙판[ENDCOLOR]을 건널 수 있습니다. 눈에서 [ICON_STRENGTH] 전투력이 +15% 증가합니다.'),
+        ('TXT_KEY_PROMOTION_ARCTICIAN_HELP',					'[COLOR_WATER_TEXT]빙하[ENDCOLOR]를 건널 수 있습니다. 눈에서 [ICON_STRENGTH] 전투력이 +15% 증가합니다.'),
         ('TXT_KEY_PEDIA_PROMOTION_ARCTICIAN',					'극지인');
