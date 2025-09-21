7e427d16eed0bd5457cfeecf0da27d0901cfbe70
HANDANI <goohandani@gmail.com>
2025-09-22 00:28:33 +0900

    모드팩 업데이트

--- AssetsMP/DLC/MP_MODSPACK/Mods/Faster Non-Aircraft Animations (v 1)/FasterNonAircraft.sql
+++
@@ -1,3 +0,0 @@
-UPDATE MovementRates SET IndividualOffset = 0; -- delay between part-units from a unit
-UPDATE MovementRates SET RowOffset = 0; -- delay between rows of part-units from a unit
-UPDATE MovementRates SET TotalTime = TotalTime/4; -- the total time a part-unit needs to reach the plot-- Insert SQL Rules Here 
